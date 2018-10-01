2b5db4f35c42d958a24d6ea2adf0a737b3af593a
HippoRun <mayradlopezh@gmail.com>
2018-09-30 18:11:09 -0600

    index js is done

--- assets/images/javascript/index.js
+++ assets/images/javascript/index.js
@@ -102,6 +102,21 @@ function handleArticleSave(){
     });
 }
 
-function handleArticleScrape
-    )
-})
+function handleArticleScrape() {
+    // This function handles the user clicking any "scrape new article" buttons
+    $.get("/api/fetch").then(function(data) {
+    // If we are able to successfully scrape the NYTIMES and compare the articles to those
+    // already in our collection, re render the articles on the page
+    // and let the user know how many unique articles we were able to save
+    initPage();
+    bootbox.alert($("<h3 class='text-center m-top-80'>").text(data.message));
+    });
+}
+
+function handleArticleClear() {
+    $.get("api/clear").then(function() {
+        articleContainer.empty();
+        initPage();
+    });
+}
+};)
